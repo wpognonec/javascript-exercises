89e50a141a5b5dc26159ae76b10aa351dc7a94b5
William Pognonec <william.pognonec@gmail.com>
2024-10-13 10:15:38 -0500

    Implemented removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  newArr = array.filter((i) => !args.includes(i))
+  return newArr
 };
 
 // Do not edit below this line
