c269abc0cc8d176daf34649db71bea783bb5a207
William Pognonec <william.pognonec@gmail.com>
2024-10-12 21:59:37 -0500

    Implemented repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num < 0) {
+        return "ERROR"
+    }
+    
+    newStr = ""
+    while (num > 0) {
+        newStr += str
+        num--
+    } 
+    return newStr
 };
 
 // Do not edit below this line
