83fc70e1b9c7194b6e3ff272b2e488d3d8cb68fa
William Pognonec <william.pognonec@gmail.com>
2024-10-13 10:37:52 -0500

    Implemented sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+  if (startNum < 0 || endNum < 0) return "ERROR";
+  if (!Number.isInteger(startNum) || !Number.isInteger(endNum)) return "ERROR";
 
+  total = 0;
+  if (startNum > endNum) [startNum, endNum] = [endNum, startNum];
+
+  for (let i = startNum; i <= endNum; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
