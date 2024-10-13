c531203c97bf39bd56952f4338189f6171fec433
William Pognonec <william.pognonec@gmail.com>
2024-10-13 09:40:56 -0500

    Implemented reverseString method

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let strArray = string.split("")
+  strArray = strArray.reverse()
+  return strArray.join("")
 };
 
 // Do not edit below this line
