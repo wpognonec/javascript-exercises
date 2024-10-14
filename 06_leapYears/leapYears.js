59f290e4c1cdaff3f94f21d8f5010909e1c1a47a
William Pognonec <william.pognonec@gmail.com>
2024-10-13 21:11:08 -0500

    Added leapYear function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if (!(year % 400)) return true
+  if (!(year % 100) && (year % 400)) return false
+  if (!(year % 4)) return true
+  return false
 };
 
 // Do not edit below this line
