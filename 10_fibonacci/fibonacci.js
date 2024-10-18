f7bba137d26905a9581b33a8c2dcf2e3af2ca182
William Pognonec <william.pognonec@gmail.com>
2024-10-18 10:08:31 -0500

    Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+  let num = parseInt(a)
+  if (num < 0) return "OOPS"
+  if (num === 0) return 0
+  let currentDigit = 1
+  let previousDigit = 1
+  for (let i = 2; i < num; i++) {
+    temp = currentDigit
+    currentDigit = currentDigit + previousDigit
+    previousDigit = temp
+  }
+  return currentDigit
 };
 
 // Do not edit below this line
