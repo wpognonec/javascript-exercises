ff0150fc8851d23c5a85a3027e0a91ec2073276d
William Pognonec <william.pognonec@gmail.com>
2024-10-18 09:34:08 -0500

    Calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  if (a.length === 0) return 0
+	return a.reduce((total, i) => total + i);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+	return a.reduce((total, i) => total * i);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (!a) return 1
+  let sum = 1
+  for (let i = a; i > 0; i--) {
+    sum *= i
+  }
+  return sum
 };
 
 // Do not edit below this line
