e4bf3c983baeab547ac18cc6a33d8ece7cd56d2a
William Pognonec <william.pognonec@gmail.com>
2024-10-18 10:23:03 -0500

    findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let oldest = people[0];
+  let today = new Date().getFullYear()
+  people.forEach(person => {
+    let age = person.yearOfDeath - person.yearOfBirth
+    let oldestAge = (oldest.yearOfDeath || today) - oldest.yearOfBirth
+    if (oldestAge < age) oldest = person
+  });
+  return oldest
 };
 
 // Do not edit below this line
