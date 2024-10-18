a8daef46d9fb184a75e7d579029474f95190a4bd
William Pognonec <william.pognonec@gmail.com>
2024-10-18 09:55:52 -0500

    Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let filteredArray = [...str.toLocaleLowerCase()].filter((chr) => !".,! ".includes(chr))
+  return filteredArray.toString() === filteredArray.reverse().toString();
 };
 
 // Do not edit below this line
